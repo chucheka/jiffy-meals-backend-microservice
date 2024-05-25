708092dfb07b0fa3319293b43b81078bc8a0dac3
= <=>
2024-05-25 14:34:10 +0100

    create other microservices

---
+++ resturant-service/src/test/java/com/chucheka/resturant_service/ResturantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chucheka.resturant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ utility-service/src/test/java/com/chucheka/utility_service/UtilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chucheka.utility_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
